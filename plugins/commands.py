a8b9ce102bd17404cfef2a63552d09e173804c22
TG Expert <78008481+v-v-r-official@users.noreply.github.com>
2021-03-25 18:08:43 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -10,14 +10,14 @@ from translation import Translation
 from Tools.Download import download
 
 my_father = "https://t.me/{}".format(Config.USER_NAME[1:])
-support = "https://telegram.dog/Ns_Bot_supporters"
+support = "https://t.me/VKP_BOTS"
 @Client.on_message(Filters.command(["start"]))
 async def start(c, m):
 
     await c.send_message(chat_id=m.chat.id,
                          text=Translation.START.format(m.from_user.first_name, Config.USER_NAME),
                          reply_to_message_id=m.message_id,
-                         reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("My Father 👨‍💻", url=my_father), InlineKeyboardButton("📌Support channel", url=support)]]))
+                         reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("⚙ CHANNEL ⚙", url=my_father), InlineKeyboardButton("⚙ GROUP ⚙", url=support)]]))
     logger.info(f"{m.from_user.first_name} used start command")
 
 
